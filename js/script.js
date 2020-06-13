8b919194286061b03727c939c5813ed639f96a66
Tee15 <temitopedml@gmail.com>
2020-06-12 21:21:43 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function check(form){
+    var username = document.getElementById("username")
+    var password = document.getElementById("password")
+    if (username == "username1" && password == "password1"){
+        alert("Login sucessfully");
+    }
+    else
+    [
+        alert("Login sucessfully")
+    ]
+}
+
+ 
