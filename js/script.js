f18959e9c633de907c5e1e09f06fb7ce61efcc59
Tee15 <temitopedml@gmail.com>
2020-06-15 13:14:00 -0700

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 function check(form){
-    var username = document.getElementById("username")
-    var password = document.getElementById("password")
-    if (username == "username1" && password == "password1"){
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+    console.log(username)
+    if (username === "username1" && password === "password1"){
+        console.log("hello")
         alert("Login sucessfully");
     }
-    else
-    [
-        alert("Login sucessfully")
-    ]
+    else{  
+        alert("Invalid Username or password")
+    }    
 }
+    
+    
+
 
  
